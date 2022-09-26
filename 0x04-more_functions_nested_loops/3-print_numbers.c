c024fa238399b5ea568f1652279535f71d5a46f9
hayes28 <5549@holbetonschools.com>
2022-09-26 15:20:38 -0700

    update

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers from 0-9
+ * followed by a new line
+ */
+void print_numbers(void)
+{
+	int N = '0';
+
+	for (;N <= '9'; N++)
+		_putchar('N');
+	_putchar('\n');
+
+}
