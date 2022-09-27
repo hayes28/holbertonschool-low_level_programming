636f691797bd4f214c3f9510e5dcac17ed2b3253
hayes28 <5549@holbetonschools.com>
2022-09-27 15:15:40 -0700

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,7 +5,7 @@
  *
  * Return: 0
  */
-int main (void)
+int main(void)
 {
 	int n;
 
@@ -14,13 +14,13 @@ int main (void)
 		if ((n % 3) == 0 && (n % 5) != 0)
 			printf(" Fizz");
 		else if ((n % 5) == 0 && (n % 3) != 0)
-			printf( "Buzz");
+			printf(" Buzz");
 		else if ((n % 3) == 0 && (n % 5) == 0)
 			printf(" FizzBuzz");
 		else if (n == 1)
-			printf(%d, n);
+			printf("%d", n);
 		else
-			printf(' %d, n);
+			printf(" %d", n);
 	}
 	printf("\n");
 
