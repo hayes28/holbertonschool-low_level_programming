6a219b015d86ba954cd7f8ed3aaa7288163cb6bb
hayes28 <5549@holbetonschools.com>
2022-09-27 20:54:50 -0700

    update

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef main_h
+#define main_h
+
+/**
+ * main - main.h
+ *
+ * Description: header file containing decarations
+ * for all functions for 0x05. C - Pointers, arrays and strings
+ */
+
+#include <stdio.h>
+#include <ctype.h>
+
+void reset_to_98(int *n);
+
+int _putchar(char c);
+#endif
