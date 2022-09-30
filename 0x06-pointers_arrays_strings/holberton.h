589b502a7324b72b4868a1023c421a032a807f52
hayes28 <heather.hayes918@gmail.com>
2022-09-30 14:54:04 -0700

    Header file

---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,18 @@
+#ifndef holberton_h
+#define holberton_h
+
+/**
+ * holberton - holberton.h
+ *
+ * Description: header file containing the prototypes
+ * for all the functions in 0x06. C - more pointers, arrays
+ * and strings.
+ */
+
+#include <stdio.h>
+#include <ctype.h>
+
+
+int _putchar(char c);
+
+#endif
