f40be9bcd7549e0a24f4b4bcaa874fe77db6e01f
hayes28 <heather.hayes918@gmail.com>
2022-10-04 17:22:08 -0700

    adding files

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+README for project 0x07. C - Even more pointers, arrays and strings.
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,22 @@
+#ifndef main_h
+#define main_h
+
+/**
+ * Main - main.h
+ *
+ * Description: Header file containing the prototypes
+ * for all of the fuhnctions in 0x07. C - Even more pointers, arrays
+ * and strings
+ */
+
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+
+#endif
