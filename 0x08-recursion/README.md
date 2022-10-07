2d6274f56d9951b74ccdc088c68e510451555b3f
hayes28 <heather.hayes918@gmail.com>
2022-10-06 17:50:49 -0700

    README

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+README for project 0x08. C - Recursion
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,19 @@
+#ifndef main_h
+#define main_h
+
+/**
+ * Main - main.h
+ *
+ * Description: Header file containing the prototypes
+ * for all of the functions for project 0x08. C - Recursion.
+ */
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+
+#endif
