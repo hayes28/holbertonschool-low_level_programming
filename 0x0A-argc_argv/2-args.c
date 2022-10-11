bf904d089f3d41d4f9f3b21840de7988fb1bae05
hayes28 <heather.hayes918@gmail.com>
2022-10-10 18:36:15 -0700

    2-args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - program that prints all arguments it receives.
+ * @argc: counts of arguments passed
+ * @argv: array of passed arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i > argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
