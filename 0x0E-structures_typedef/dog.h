ee5b7b20a9df95360d505e5acc48d9aeeaaab5b6
hayes28 <heather.hayes918@gmail.com>
2022-10-24 14:56:30 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - Data type for a dog.
+* @name: dog's name
+* @age: dog's age
+* @owner: dog's owner
+*/
+
+typedef struct 
+{
+char *name;
+float age;
+char *owner;
+} Dog;
+
+#endif
