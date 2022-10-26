17604b91ebb10de13f245b8cbb476b350422e552
hayes28 <heather.hayes918@gmail.com>
2022-10-25 17:39:04 -0700

    header and README

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,19 @@
+#ifndef variadic_functions_h
+#define variadic_functions_h
+
+/**
+* variadic_functions_h - this is my header file for this project
+*
+* Description - Header file contaning the prototypes
+* for all the functions for this project
+*/
+
+/* PROTOTYPES */
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
