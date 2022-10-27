ada419a7eae4b0e888dab990c3415e652da6d5c0
hayes28 <heather.hayes918@gmail.com>
2022-10-27 12:15:37 -0700

    4-new

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,60 +1,61 @@
 #include "dog.h"
-char *_strdup(const char *str);
+char *_strdup(char *str);
 /**
-* new_dog - Function to create a new dog
-* @name: name of new dog
-* @age: age of new dog
+* new_dog - creates a new dog
+* @name: new dog's name
+* @age: new dog's age
 * @owner: owner of new dog
 *
-* Return: NULL if function fails
+* Return: pointer to the new struct
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *fi;
+	dog_t *new;
 
-	fi = malloc(sizeof(dog_t));
-	if (fi)
+	new = malloc(sizeof(dog_t));
+	if (new == NULL)
+		return (NULL);
 
-	fi->name = _strdup(name);
-	if (fi->name)
+	new->name = _strdup(name);
+	if (new->name == NULL)
 	{
-		free(fi);
+		free(new);
 		return (NULL);
 	}
-	fi->owner = _strdup(owner);
-	if (fi->owner)
+	new->owner = _strdup(owner);
+	if (new->owner == NULL)
 	{
-	free(fi->name);
-	free(fi);
-	return (NULL);
+		free(new->name);
+		free(new);
+		return (NULL);
 	}
-	fi->age = age;
+	new->age = age;
 
-	return (fi);
-
+	return (new);
 }
 
 /**
-* _strdup - copies string while allocating memory.
-* @str: pointer to a string
+* _strdup - copies string to allocate memory
+* @str: pointer to string
 *
-* Return: pointer to copied string, otherewise NULL if fails.
+* Return: pointer to copy string, NULL if NULL or error with malloc
 */
-char *_strdup(const char *str)
+char *_strdup(char *str)
 {
-	int size, a;
+	int size, i;
 	char *p;
 
+	if (str == NULL)
+		return (NULL);
 	for (size = 0; str[size] != 0; size++)
-
-	p = malloc(sizeof(char) * (size + 1));
-	if (p)
-
-	for (a = 0; a < size; a++)
+	p = malloc(sizeof(char) * (size));
+	if (p == NULL)
+		return (NULL);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
 	{
-		p[a] = str[a];
-		a++;
+		p[i] = str[i];
 	}
-	p[a] = '\0';
 	return (p);
 }
