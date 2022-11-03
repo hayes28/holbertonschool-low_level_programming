f876064494812148ee535a3df71a4c35e722c4e3
hayes28 <heather.hayes918@gmail.com>
2022-11-03 11:31:30 -0700

    3-calc

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,12 +21,10 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (ops[i].op != NULL)
+	while (ops[i].op)
 	{
-		if (*ops[i].op == *s)
-		{
+		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
-		}
 		i++;
 	}
 	return (NULL);
