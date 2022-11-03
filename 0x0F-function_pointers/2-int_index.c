7cf1178f4960659367511c1597374ceef5578278
hayes28 <heather.hayes918@gmail.com>
2022-11-02 20:35:36 -0700

    2-int

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,17 +12,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL && cmp == NULL && size <= 0)
-		return (-1);
-
-	if (size > 0)
+	if (array && cmp)
 	{
 		for (i = 0; i < size; i++)
 		{
-			if (cmp(array[i]))
-			{
+			if (cmp(array[i]) != 0)
 				return (i);
-			}
 		}
 	}
 	return (-1);
