4b41fac7505c861ef86c98dabaa69716f9912eb6
hayes28 <heather.hayes918@gmail.com>
2022-11-06 16:47:31 -0800

    update on 9-insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,6 +30,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		tmp = tmp->next;
 	   count++;
 	}
+	new = malloc(sizeof(listint_t));
+	if (tmp == NULL)
+	{
+		free(tmp);
+		return (NULL);
+	}
 	new->next = tmp->next;
 	tmp->next = new;
 	new->n = n;
