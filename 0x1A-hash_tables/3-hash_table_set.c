2f70292bd4a76913ef75246d7848444046652bde
hayes28 <heather.hayes918@gmail.com>
2022-11-16 13:54:29 -0800

    hash table set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,4 +1,5 @@
 #include "hash_tables.h"
+#include <string.h>
 
 /**
  * hash_table_set - function that adds
@@ -13,23 +14,21 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new = malloc(sizeof(hash_node_t));
+	hash_node_t *new;
 	unsigned long int index;
 
-	if (!new)
+	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
-	if (!key || !value || !ht)
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	new = malloc(sizeof(hash_node_t));
+
+	if (new == NULL)
 		return (0);
 
 	new->key = strdup(key);
 	new->value = strdup(value);
-	index = key_index((const unsigned char *)key, ht->size);
-
-	if (ht->array[index] == NULL)
-	{
-		ht->array[index] = new;
-		return (1);
-	}
 	new->next = ht->array[index];
 	ht->array[index] = new;
 
