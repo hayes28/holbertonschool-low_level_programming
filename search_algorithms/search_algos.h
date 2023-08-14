63d975a72175bc5fa8cc9455cf92430ecf2ced9c
HH <heather.hayes918@gmail.com>
2023-08-13 20:50:07 -0500

    Last try I swear

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -12,7 +12,6 @@
  * @next: Pointer to the next node
  * @express: Pointer to the next node in the express lane
  */
-
 typedef struct skiplist_s
 {
     int n;
@@ -28,5 +27,6 @@ int binary_search(int *array, size_t size, int value);
 int recursive_binary(int *array, size_t l, size_t r, int value);
 int advanced_binary(int *array, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
+
 
 #endif /* SEARCH_ALGOS_H */
