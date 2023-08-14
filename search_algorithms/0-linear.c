c6e1d0b859f3de4c88460d4c0f270fcae3326588
HH <heather.hayes918@gmail.com>
2023-08-13 19:31:22 -0500

    Maybe it wants a extra line?

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -19,12 +19,11 @@ int linear_search(int *array, size_t size, int value)
     if (array == NULL)
         return (-1);
 
-    for (i = 0 ; i < size ; i++)
+    for (i = 0; i < size; i++)
     {
         printf("Value checked array[%lu] = [%d]\n", i, array[i]);
         if (array[i] == value)
             return (i);
-
     }
     return (-1);
 }
