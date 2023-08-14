e770a53c44b60bc0abd37927f571140484f01b92
HH <heather.hayes918@gmail.com>
2023-08-13 20:24:06 -0500

    Task 0-1

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -10,20 +10,18 @@
  * Return: first index where value is located or -1 if value is not present
  * in array or if array is NULL
  */
-
 int linear_search(int *array, size_t size, int value)
-
 {
-    size_t i;
+	size_t i;
 
-    if (array == NULL)
-        return (-1);
+	if (array == NULL)
+		return (-1);
 
-    for (i = 0; i < size; i++)
-    {
-        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return (i);
-    }
-    return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
